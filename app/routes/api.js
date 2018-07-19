342b794ac674de96c51396e802e399cb940993f1
Woo Yun <hh747hh@gmail.com>
2018-07-19 12:56:20 -0400

    created index.html/added angularjs/bootstrap

--- app/routes/api.js
+++ app/routes/api.js
@@ -1,34 +1,32 @@
 var User = require("../models/user");
 
-module.exports = {
+module.exports = function(router) {
+  //http://localhost:5000/users
+  router.post("/users", function(req, res) {
+    var user = new User();
+    user.username = req.body.username;
+    user.password = req.body.password;
+    user.email = req.body.email;
+    invalid =
+      req.body.username === null ||
+      req.body.username === "" ||
+      req.body.password === null ||
+      req.body.password === "" ||
+      req.body.email === null ||
+      req.body.email === "";
 
-//http://localhost:5000/users
-app.post("/users", function(req, res) {
-  var user = new User();
-  user.username = req.body.username;
-  user.password = req.body.password;
-  user.email = req.body.email;
-  invalid =
-    req.body.username === null ||
-    req.body.username === "" ||
-    req.body.password === null ||
-    req.body.password === "" ||
-    req.body.email === null ||
-    req.body.email === "";
+    if (invalid) {
+      res.send("Email, Password and Username must be provided");
+    } else {
+      user.save(function(err) {
+        if (err) {
+          res.send("UserName or Email already exist.");
+        } else {
+          res.send("User created!");
+        }
+      });
+    }
+  });
 
-  if (invalid) {
-    res.send("Email, Password and Username must be provided");
-  } else {
-    user.save(function(err) {
-      if (err) {
-        res.send("UserName or Email already exist.");
-      } else {
-        res.send("User created!");
-      }
-    });
-  }
-});
-
-
-
-}
+  return router;
+};
--- server.js
+++ server.js
@@ -4,10 +4,15 @@ var port = process.env.PORT || 5000;
 var morgan = require("morgan");
 var mongoose = require("mongoose");
 var bodyParser = require("body-parser");
+var router = express.Router();
+var appRoutes = require("./app/routes/api")(router);
+var path = require("path");
 
+app.use(morgan("dev"));
 app.use(bodyParser.json()); //for parsing application/json
 app.use(bodyParser.urlencoded({ extended: true })); // parse application/x-www-form-urlencoded
-app.use(morgan("dev"));
+app.use(express.static(__dirname + "/public"));
+app.use("/api", appRoutes);
 
 //DB Config
 const db = require("./config/keys").mongoURI;
@@ -24,6 +29,10 @@ mongoose
 // app.get("/home", function(req, res) {
 //   res.send("Hello home");
 // });
+
+app.get("*", function(req, res) {
+  res.sendFile(Path.join(__dirname + "/public/app/views/index.html"));
+});
 
 app.listen(port, function() {
   console.log("Running the server on port " + port);
